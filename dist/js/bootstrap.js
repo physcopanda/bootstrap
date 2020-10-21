f8d5571c5448f4b8e1fe5adbb01c04cd6a3b9a58
Jamie Howard <webdev@rgcd.co.uk>
2020-10-21 17:42:12 +0100

    fix click affix bug #release

--- dist/js/bootstrap.js
+++ dist/js/bootstrap.js
@@ -2243,7 +2243,7 @@ if (typeof jQuery === 'undefined') {
     var position     = this.$element.offset()
     var targetHeight = this.$target.height()
 
-    if (offsetTop != null && this.affixed == 'top') return scrollTop < offsetTop ? 'top' : false
+    if (offsetTop != null && this.affixed == 'top') return scrollTop <= offsetTop ? 'top' : false
 
     if (this.affixed == 'bottom') {
       if (offsetTop != null) return (scrollTop + this.unpin <= position.top) ? false : 'bottom'
